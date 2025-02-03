eb5e6c7d00fc3b81b23eee65c0f82095ca813922
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-02-03 18:16:50 +0330

    نمایش وضیعت پلن کاربر

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\helper\helper;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\User\UserResource;
+use App\Http\Resources\CustomResource;
 use App\Models\Content;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,7 +21,14 @@ class UserController extends Controller
     {
         $totalgeneratedword = Content::select('count')->where('vendor_id', $request->user()->id)->sum('count');
         $totalcontent = Content::where('vendor_id', $request->user()->id)->get()->count();
-        $dd=@helper::checkPlan($request->user()->id)->original;
-dd($dd);
+        $original=@helper::checkPlan($request->user()->id)->original;
+        $data = [
+            'totalgeneratedword' => $totalgeneratedword,
+            'totalcontent' => $totalcontent,
+            'original' => $original,
+        ];
+
+        return $this->respondWithResource(new CustomResource($data));
+
     }
 }
